abf8779ebfa54dec336dba5da4196e5172a182a8
Andrew Regan <aregan@gmail.com>
2006-06-20 00:40:57 +0000

    no message
    git-svn-id: file:///Users/andrewregan/Development/SVN/repos/trunk/Bloggers4Labour@32 aa2b285a-0eb0-49ca-9bdb-c93b1c50f45f

---
+++ src/org/bloggers4labour/recommend/RecommendationResult.java
@@ -0,0 +1,19 @@
+/*
+ * RecommendationResult.java
+ *
+ * Created on 20 June 2006, 00:45
+ *
+ * To change this template, choose Tools | Template Manager
+ * and open the template in the editor.
+ */
+
+package org.bloggers4labour.recommend;
+
+/**
+ *
+ * @author andrewre
+ */
+public enum RecommendationResult
+{
+	OK, DUPLICATE, BAD_INPUTS, ERROR
+}
