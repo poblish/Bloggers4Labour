07e7b148d3d572f819aa4f64d71eb545a45c1644
Andrew Regan <aregan@gmail.com>
2006-06-24 01:56:46 +0000

    New class to encapsulate vote count and previous status value
    git-svn-id: file:///Users/andrewregan/Development/SVN/repos/trunk/Bloggers4Labour@36 aa2b285a-0eb0-49ca-9bdb-c93b1c50f45f

--- src/org/bloggers4labour/recommend/RecommendationResult.java
+++ src/org/bloggers4labour/recommend/RecommendationResult.java
@@ -1,7 +1,7 @@
 /*
  * RecommendationResult.java
  *
- * Created on 20 June 2006, 00:45
+ * Created on 21 June 2006, 21:15
  *
  * To change this template, choose Tools | Template Manager
  * and open the template in the editor.
@@ -13,7 +13,37 @@ package org.bloggers4labour.recommend;
  *
  * @author andrewre
  */
-public enum RecommendationResult
+public class RecommendationResult
 {
-	OK, DUPLICATE, BAD_INPUTS, ERROR
+	private RecommendationStatus	m_Status;
+	private long			m_VoteCount;
+
+	/********************************************************************
+	********************************************************************/
+	public RecommendationResult( RecommendationStatus inStatus, long inVoteCount)
+	{
+		m_Status = inStatus;
+		m_VoteCount = inVoteCount;
+	}
+
+	/********************************************************************
+	********************************************************************/
+	public static RecommendationResult newErrorResult()
+	{
+		return new RecommendationResult( RecommendationStatus.ERROR, -1L);
+	}
+
+	/********************************************************************
+	********************************************************************/
+	public RecommendationStatus getStatus()
+	{
+		return m_Status;
+	}
+
+	/********************************************************************
+	********************************************************************/
+	public long getVoteCount()
+	{
+		return m_VoteCount;
+	}
 }
