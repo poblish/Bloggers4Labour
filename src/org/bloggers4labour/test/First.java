53193995e8340b68acc1e07f14a936398ee16370
Andrew Regan <aregan@gmail.com>
2006-08-19 20:01:37 +0000

    no message
    git-svn-id: file:///Users/andrewregan/Development/SVN/repos/trunk/Bloggers4Labour@43 aa2b285a-0eb0-49ca-9bdb-c93b1c50f45f

--- src/org/bloggers4labour/test/First.java
+++ src/org/bloggers4labour/test/First.java
@@ -12,8 +12,13 @@ package org.bloggers4labour.test;
 
 import de.nava.informa.core.*;
 import de.nava.informa.impl.basic.ChannelBuilder;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Locale;
 import java.util.Observable;
 import java.util.Observer;
+import java.util.TimeZone;
 import org.bloggers4labour.feed.*;
 import org.bloggers4labour.*;
 
@@ -27,12 +32,35 @@ public class First implements Observer
 
 	public static void main( String[] args)
 	{
-		java.math.BigInteger	bi = new java.math.BigInteger("0");
+		Calendar		theStartCal = Calendar.getInstance( Locale.UK );
+		Calendar		theEndCal = Calendar.getInstance( Locale.UK );
+		DateFormat		dtf = DateFormat.getDateTimeInstance( DateFormat.LONG, DateFormat.LONG, Locale.UK);
+		SimpleDateFormat	sdf = new SimpleDateFormat( "yyyy-MM-dd HH:mm:ss", Locale.US);
+
+		sdf.setTimeZone( TimeZone.getTimeZone("America/New_York") );
+
+		int	numWeeksback = 0;
+		int	curWeekDay = theStartCal.get( Calendar.DAY_OF_WEEK );
+		int	diff = curWeekDay - theStartCal.getFirstDayOfWeek();
+
+		theStartCal.set( Calendar.HOUR, 0);
+		theStartCal.set( Calendar.MINUTE, 0);
+		theStartCal.set( Calendar.SECOND, 0);
+		theStartCal.set( Calendar.MILLISECOND, 0);
+
+		long	theNewMSecs = theStartCal.getTimeInMillis() - (( numWeeksback * 7 + diff) * 86400 * 1000);
+		theStartCal.setTimeInMillis(theNewMSecs);
+		theEndCal.setTimeInMillis( theNewMSecs + ( 7 * 86400 * 1000));
+
+		System.out.println(">= " + sdf.format( theStartCal.getTime() ) + " or " + dtf.format( theStartCal.getTime() ));
+		System.out.println(" < " + sdf.format( theEndCal.getTime() ) + " or " + dtf.format( theEndCal.getTime() ));
+
+/*		java.math.BigInteger	bi = new java.math.BigInteger("0");
 
 		bi = bi.setBit(58).setBit(49).setBit(150);
 
 		System.out.println("==> " + bi.toString( Character.MAX_RADIX ));
-
+*/
 		// s_FL.addObserver( new First() );
 	}
 
