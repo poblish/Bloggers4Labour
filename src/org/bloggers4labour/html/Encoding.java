c96fc4dab55da11abd5c31a9583f8b805d1b363f
Andrew Regan <aregan@gmail.com>
2006-08-28 22:18:15 +0000

    Fixed, though I probably don't need it after all...
    git-svn-id: file:///Users/andrewregan/Development/SVN/repos/trunk/Bloggers4Labour@48 aa2b285a-0eb0-49ca-9bdb-c93b1c50f45f

--- src/org/bloggers4labour/html/Encoding.java
+++ src/org/bloggers4labour/html/Encoding.java
@@ -54,13 +54,73 @@
 
 package org.bloggers4labour.html;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.regex.*;
+import org.apache.log4j.Logger;
+
 /**
  *
  * @author andrewre
  */
 public class Encoding	// Formerly com.purpletech.util.Purple_Utils
 {
-    /**
+	private static Logger	s_Logger = Logger.getLogger("Main");
+
+	public static void main( String[] args)
+	{
+		String[]	testStrings = {
+				"takes, please get in touch.<a href=\"http://www.recessmonkey.com\"><img src=\"http://bill.verity-networks.com/ext/recess/media/votemonkey.gif\" alt=\"Recess Monkey\" title=\"Recess Monkey\" /></a></p>",
+				"This evening's meeting heard reports on: The A2 - Blackheath to <st1:Street w:st=\"on\"><st1:address w:st=\"on\">Shooters Hill Rd</st1:address></st1:Street> Coach f...",
+				"arse<a href=\"http://www.tribute.r8.org\" /> <b> Woot</b>!!</a>!!",
+				"<P><!--StartFragment -->Sophia over at&nbsp;<A href=\"http://www.demosgreenhouse.co.uk/archives/000970.html\">Demos</A> has been reflecting on whether there might be lessons to be learned from some bits of the private sector, in particular <A href=\"http://www.pret.com/\">Pret-a-Manger</A>.&nbsp; Im intrigued by some of the ideas (without being clear whether theyre practical in a local government setting), for example:</P>",
+				"Chuckie Kennedy party of <strike>not very</strike> prestigious jobs",
+				"<P class=MsoNormal style=\"MARGIN: 0cm 0cm 0pt\"><TT><SPAN style=\"FONT-SIZE: 10pt; FONT-FAMILY: 'Lucida Sans Typewriter'\"><FONT face=\"Arial, Helvetica, sans-serif\">A&nbsp;really interesting email from a member of the Hastings Creative and Media Community&nbsp;came to my attention today, about what is claimed to be ‘the 1<SUP>st</SUP> genuinely independent public access web tv station in Hasings’.<?xml:namespace prefix = o ns = \"urn:schemas-microsoft-com:office:office\" /><o:p></o:p></FONT></SPAN></TT></P>\n<P class=MsoNormal style=\"MARGIN: 0cm 0cm 0pt\"><TT><SPAN style=\"FONT-SIZE: 10pt; FONT-FAMILY: 'Lucida Sans Typewriter'\"><FONT face=\"Arial, Helvetica, sans-serif\">&nbsp;<o:p></o:p></FONT></SPAN></TT></P>\n<P class=MsoNormal style=\"MARGIN: 0cm 0cm 0pt\"><TT><SPAN style=\"FONT-SIZE: 10pt; FONT-FAMILY: 'Lucida Sans Typewriter'\"><FONT face=\"Arial, Helvetica, sans-serif\">Its presently posted </FONT><A href=\"http://www.angusg.f2s.com/\"><FONT face=\"Arial, Helvetica, sans-serif\">here</FONT></A><FONT face=\"Arial, Helvetica, sans-serif\"> but from tomorrow will be running from:</FONT></SPAN></TT></P>\n<P class=MsoNormal style=\"MARGIN: 0cm 0cm 0pt\"><TT><SPAN style=\"FONT-SIZE: 10pt; FONT-FAMILY: 'Lucida Sans Typewriter'\"><FONT face=\"Arial, Helvetica, sans-serif\">&nbsp;<o:p></o:p></FONT></SPAN></TT></P>\n<P class=MsoNormal style=\"MARGIN: 0cm 0cm 0pt\"><TT><SPAN style=\"FONT-SIZE: 10pt; FONT-FAMILY: 'Lucida Sans Typewriter'\"><A href=\"http://www.tvhastings.org/\"><FONT face=\"Arial, Helvetica, sans-serif\">www.tvhastings.org</FONT></A><o:p></o:p></SPAN></TT></P>\n<P class=MsoNormal style=\"MARGIN: 0cm 0cm 0pt\"><TT><SPAN style=\"FONT-SIZE: 10pt; FONT-FAMILY: 'Lucida Sans Typewriter'\"><FONT face=\"Arial, Helvetica, sans-serif\">&nbsp;<o:p></o:p></FONT></SPAN></TT></P><TT><SPAN style=\"FONT-SIZE: 10pt; FONT-FAMILY: 'Lucida Sans Typewriter'; mso-ansi-language: EN-GB; mso-fareast-language: EN-US; mso-bidi-language: AR-SA\"><FONT face=\"Arial, Helvetica, sans-serif\">Its early days but all credit to the creators - I’m sure it can’t fail to be better than the standard of the local dead tree press.</FONT></SPAN></TT>",
+				"spotted at<a href=\"http://foo.com\"> normblog</a>.",
+				"spotted at <a href=\"http://foo.com\">normblog </a>Hullo",
+				"<p>This site lists Blogs in the UK. Woo.<BR><br  /><a href=\"http://www.britblog.com\"  target=\'_blank\'><p style=\"text-align:center;\"><img src=\"http://www.westminstervillage.co.uk/images/icon_britblog_80x15.gif\" border=\"0\" title=\"\" alt=\"\" class=\"pivot-image\" /></p></a></p>",
+				"rewind<a href=\"#footnote-1-1581\" id=\"footnote-link-1-1581\" title=\"See the footnote.\"><strike>fldflfslsjfs</strike></a> end.",
+				"<div class=\"feedflare\"><a href=\"http://feeds.feedburner.com/~f/wongablog?a=ylCzK0wE\"><img src=\"http://feeds.feedburner.com/~f/wongablog?i=ylCzK0wE\" border=\"0\"></img></a> <a href=\"http://feeds.feedburner.com/~f/wongablog?a=msj6myka\"><img src=\"http://feeds.feedburner.com/~f/wongablog?i=msj6myka\" border=\"0\"></img></a> <a href=\"http://feeds.feedburner.com/~f/wongablog?a=iOpV25QU\"><img src=\"http://feeds.feedburner.com/~f/wongablog?i=iOpV25QU\" border=\"0\"></img></a> <a href=\"http://feeds.feedburner.com/~f/wongablog?a=rV8omHpX\"><img src=\"http://feeds.feedburner.com/~f/wongablog?i=rV8omHpX\" border=\"0\"></img></a></div>",
+				"<a href=\"http://feeds.feedburner.com/~f/wongablog?a=ylCzK0wE\"><img src=\"http://feeds.feedburner.com/~f/wongablog?i=ylCzK0wE\" border=\"0\"></img></a>",
+				"<a href=\"http://feeds.feedburner.com/~f/wongablog?a=ylCzK0wE\"><img src=\"http://feeds.feedburner.com/~f/wongablog?i=ylCzK0wE\" border=\"0\" /></a>",
+				"<p><a id=\"more-788\"></a><center><a href=\"http://www.flickr.com/photos/josalmon/86375059/\"><img src=\"http://static.flickr.com/41/86375059_71b88aaa15.jpg\" width=\"375\" height=\"500\" alt=\"Pitt Rivers Museum, Oxford\" title=\"Pitt Rivers Museum, Oxford\" /></a></center></p>",
+				"<div xmlns=\"http://www.w3.org/1999/xhtml\"><div align=\"left\"><a href=\"http://photos1.blogger.com/blogger/6247/500/1600/ramsrightlogowhite.jpg\"><img alt=\"\" border=\"0\" height=\"197\" src=\"http://photos1.blogger.com/blogger/6247/500/320/ramsrightlogowhite.jpg\" style=\"DISPLAY: block; MARGIN: 0px auto 10px; WIDTH: 181px; CURSOR: hand; HEIGHT: 137px; TEXT-ALIGN: center\" width=\"246\"/></a><br/><strong>                               Tooting and Mitcham v Ramsgate Sat 14th January</strong></div><div align=\"left\"><strong/><br/>Third placed Ramsgate visited second placed Tooting this afternoon, and the strength and pace of both teams produced an entertaining fixture that showed why they are both near the top.<br/>The first chance of the match fell to Ramsgate in the 2nd minute, when Shaun Welford's header went just over the bar. The next 6 minutes saw 6 corners - the first 2 falling to Tooting and the remainder to the Rams, the last one being fumbled by 'keeper Scarcella to allow Stuart Vahid to flick the ball into the net in the 8th minute.<br/>A stunning 'Banks-style' save by Plumley from Mikael Munday at point blank range in the 64th minute retained the Rams' lead in a hard fought 2nd half, whilst in the 80th minute Dave Hastings went down in the Rams' area under a challenge from Warren Schulz, only to receive a yellow card rather than the hoped for penalty. Two minutes into stoppage time Hastings went down in the area between Edd Vahid and Liam Morris, and this time was rewarded with a spot kick that Danny Twin converted for a late equaliser. </div><div align=\"left\"> </div><div align=\"left\">Next Home Match is Sat 21st Jan 06 - 3PM KO against Molesey</div></div>",
+				"&#60;i&#62;For as long as we are subjects, not citizens, of our country&#60;/i&#62;Have a <i><b>woo!</b></i>, yeah!",
+				"<a href=\"XXX\"><img src=\"YYY.gif\" /></a>",
+				"I co-chair the neighbourhood management forum for Heathside and <?xml:namespace prefix=st1 ns= \"urn:schemas-microsoft-com:office:smarttags\" />Lethbridge with Tracey Skingly",
+				"<p><a href=\"http://www.amazon.co.uk/exec/obidos/ASIN/B000024V1U/madmusinofme-21\"><img src=\"http://www.madmusingsof.me.uk/archives/suburbs.jpeg\" border=\"1\" title=\"Sound of the Suburbs\"alt=\"Sound of the Suburbs\" hspace=\"3\" vspace=\"3\"></a>&nbsp;A compilation of songs taken from the Golden Age of Pop",
+				"<img alt=\"New Labour MPs\" src=\"http://www.andrewregan.com/images/mp/welcome.jpg\"/> Or, \"Yet another newspaper (and blogger) jumps on the bandwagon\"...<br/>\n<a href=\"http://www.mirror.co.uk/news/tm_objectid=15512533%26method=full%26siteid=94762%26headline=the-great-su-doku-challenge-name_page.html\">Have a go yourself</a> then send in your completed entry - plus a note of how long it took you - to see if you've beaten that legendary brainbox, <span style=\"font-weight:bold;\">Carol Vorderman</span> (an <a href=\"http://en.wikipedia.org/wiki/Alan_Turing\">Alan Turing</a> for a simpler age).<br/>\n<br/>I won't reveal the solution (unless you donate to the site!), but I will say that it took me <span style=\"font-weight:bold;\">15 minutes and 50 seconds.</span>\n\n<br/>\n<br/>Actually I must admit that I've been doing a hell of a lot of these recently. <a href=\"http://www.griffiths-jones.co.uk/sudoku/\">This site</a> offers a new one every day, plus there's an archive of easy, medium, hard, and very hard ones going back a month or two. You can also <a href=\"http://www.griffiths-jones.co.uk/sudoku/downloads.html\">download</a> a grid (as a PDF), which is just the right size for printing.<br/>\n<br/>If you still don't know what SuDoku is, look it up at the <a href=\"http://en.wikipedia.org/wiki/Sudoku\">Wikipedia</a>.",
+				"<div xmlns=\"http://www.w3.org/1999/xhtml\">Here's Celia, with other new Labour MPs at Westminster today:<br/><br/><img alt=\"New Labour MPs\" src=\"http://www.andrewregan.com/images/mp/welcome.jpg\"/>Andrew</div>",
+				"<a name=\"c111529093173103484\" />Hilary Wade",
+				"A reminder: Vote early and vote often.<sup class=\"foo\" >*</sup><SUP   >*</sup>Where often is defined as more than zero times and not more than one time.",
+				"I've only just caught up with this <u class=\"foo\" title=\"bar\" >splendid</u> piece by Michael Lynch in the Chronicle of Higher Education.",
+				"<a href=\"apple.com\">Apple</a> same way as ...<b><abbr class=\"col\" title=\"Tosser Ex-Manager\">WTEM</ABBR>-Hello <a href=\"ms.com\">MS</a>  bye",
+				"<div xmlns=\"http://www.w3.org/1999/xhtml\"><p><style type=\"text/css\">.flickr-photo { border: solid 2px #000000; }.flickr-yourcomment { }.flickr-frame { text-align: left; padding: 3px; }.flickr-caption { font-size: 0.8em; margin-top: 0px; }</style><div class=\"flickr-frame\">	<a href=\"http://www.flickr.com/photos/yukihisa/12894905/\" title=\"photo sharing\"><img src=\"http://photos10.flickr.com/12894905_de034941bc.jpg\" class=\"flickr-photo\" alt=\"\" /></a><br />	<span class=\"flickr-caption\"><a href=\"http://www.flickr.com/photos/yukihisa/12894905/\">_DSC5760</a>, originally uploaded by <a href=\"http://www.flickr.com/people/yukihisa/\">yukihisa</a>.</span></div>				<p class=\"flickr-yourcomment\">	One of the things that always gives me hope is the behaviour of children - curious, energetic, demanding and generous. What we do to them as they grow is another matter - but the potential is always there.</p></p></div>",
+				"<object width=\"425\" height=\"350\"><param name=\"movie\" value=\"http://www.youtube.com/v/79T96_WFKRM\"></param><embed src=\"http://www.youtube.com/v/79T96_WFKRM\" type=\"application/x-shockwave-flash\" width=\"425\" height=\"350\"></embed></object><br /><br /><br /><br />Leonard Cohen's song from \"Dear Heather\" album, PV by his daughter, Lorca, featuring Anjani Thomas.",
+				"<p>Lets see if this works&#8230;</p>\n<p><object width=\"425\" height=\"350\"><br />\n<param name=\"movie\" value=\"http://www.youtube.com/v/z5e4L3LtYw4\"></param><embed src=\"http://www.youtube.com/v/z5e4L3LtYw4\" type=\"application/x-shockwave-flash\" width=\"425\" height=\"350\"></embed></object></p>\n<p>Hmmm&#8230; Seems to work OK, but what a lot of hoops to jump through!  I had to deactivate the WYSIWYG editing plugin AND create the post in Internet Explorer - for some reason it would not work properly in Firefox. I think I shall push my luck and try to embed the video for the single too.</p>\n<p><object width=\"425\" height=\"350\"><br />\n<param name=\"movie\" value=\"http://www.youtube.com/v/7p7XiPymhYw\"></param><embed src=\"http://www.youtube.com/v/7p7XiPymhYw\" type=\"application/x-shockwave-flash\" width=\"425\" height=\"350\"></embed></object></p>",
+				"&lt;a href=\"http://eustonmanifesto.org/joomla/content/view/72/46/\">Nice paper&lt;/a> at euston, largely agree with it. Only reccomended for those with a serious case of time on hands!"
+		};
+
+		int	i = 0;
+
+		for ( String s : testStrings)
+		{
+			String	escapedS = htmlescape( s, false);
+			String	processedS = htmlunescape(escapedS);
+
+			if (!s.equals(processedS))
+			{
+				s_Logger.info("Test string [" + i + "] failed...");
+				s_Logger.info("   BEFORE: " + s);
+				s_Logger.info("  ESCAPED: " + s);
+				s_Logger.info("    AFTER: " + processedS);
+				s_Logger.info("=============================================================================");
+			}
+
+			i++;
+		}
+	}
+/**
      * fills the left side of a number with zeros <br>
      * e.g. zerofill(14, 3) -> "014" <br>
      * e.g. zerofill(187, 6) -> "000014"
@@ -334,12 +394,13 @@ public class Encoding	// Formerly com.purpletech.util.Purple_Utils
 		{"raquo", new Integer(187)},	// right angle quote
 		{"iquest", new Integer(191)},	// inverted question mark
     };
-    static Map e2i = new HashMap();
-    static Map i2e = new HashMap();
+    static Map<String,Integer> e2i = new HashMap<String,Integer>();
+    static Map<Integer,String> i2e = new HashMap<Integer,String>();
+
     static {
 	for (int i=0; i<entities.length; ++i) {
-	    e2i.put(entities[i][0], entities[i][1]);
-	    i2e.put(entities[i][1], entities[i][0]);	    
+	    e2i.put( (String) entities[i][0], (Integer) entities[i][1]);
+	    i2e.put( (Integer) entities[i][1], (String) entities[i][0]);	    
 	}
     }
 
@@ -480,52 +541,67 @@ public class Encoding	// Formerly com.purpletech.util.Purple_Utils
 		return theCount;
 	}
 
-    /**
-     * Reverses htmlescape.
-     * @see htmlescape */
-    public static String htmlunescape(String s1) {
-	StringBuffer buf = new StringBuffer();
-	int i;
-	for (i=0; i<s1.length(); ++i) {
-	    char ch = s1.charAt(i);
-	    if (ch == '&') {
-		int semi = s1.indexOf(';', i+1);
+	/**
+	* Reverses htmlescape.
+	* @see htmlescape */
+	public static String htmlunescape(String s1)
+	{
+		StringBuilder buf = new StringBuilder( s1.length() );
 
-// Logger.getLogger("Main").info("i = " + i + ", semi = " + semi);
+//		Matcher	strEntityMatcher = Pattern.compile( "&[A-Z]+;", Pattern.CASE_INSENSITIVE).matcher(s1);
 
-		if (semi == -1) {
-		    buf.append(ch);
-		    continue;
+		for ( int i = 0; i < s1.length(); ++i)
+		{
+			char ch = s1.charAt(i);
+			if (ch == '&')
+			{
+				int semi = s1.indexOf(';', i+1);
+
+				// s_Logger.info("i = " + i + ", semi = " + semi);
+
+				if (semi == -1)
+				{
+					buf.append(ch);
+					continue;
+				}
+
+				String entity = s1.substring(i+1, semi); // -1);
+
+				// s_Logger.info("entity=\"" + entity + "\"");
+
+				Integer iso;
+				if ( entity.charAt(0) == '#')
+				{
+					// s_Logger.info("sub=\"" + entity.substring(1) + "\"");
+
+					iso = new Integer( entity.substring(1) );
+				}
+				else
+				{
+					iso = (Integer)e2i.get(entity);
+				}
+
+				////////////////////////////////////////////////
+
+				if ( iso == null)
+				{
+					buf.append("&" + entity + ";");
+				}
+				else
+				{
+					buf.append((char) iso.intValue() );
+
+					i = semi;	// (AGR) 28 August 2006
+				}
+			}
+			else
+			{
+				buf.append(ch);
+			}
 		}
-		String entity = s1.substring(i+1, semi); // -1);
 
-Logger.getLogger("Main").info("entity=\"" + entity + "\"");
-
-		Integer iso;
-		if (entity.charAt(0) == '#') {
-
-Logger.getLogger("Main").info("sub=\"" + entity.substring(1) + "\"");
-
-		    iso = new Integer(entity.substring(1));
-Logger.getLogger("Main").info("iso = " + iso);
-		}
-		else {
-		    iso = (Integer)e2i.get(entity);
-		}
-		if (iso == null) {
-Logger.getLogger("Main").info("!!!!!!! " + entity);
-		    buf.append("&" + entity + ";");
-		}
-		else {
-		    buf.append((char)(iso.intValue()));
-		}
-	    }
-	    else {
-		buf.append(ch);
-	    }
+		return buf.toString();
 	}
-	return buf.toString();
-    }
 
     /**
      *  Filter out Windows and Mac curly quotes, replacing them with
