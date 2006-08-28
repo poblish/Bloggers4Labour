c46a554c74983358c7820e9135080ca232748b89
Andrew Regan <aregan@gmail.com>
2006-08-28 22:46:54 +0000

    no message
    git-svn-id: file:///Users/andrewregan/Development/SVN/repos/trunk/Bloggers4Labour@49 aa2b285a-0eb0-49ca-9bdb-c93b1c50f45f

---
+++ src/org/bloggers4labour/tag/Blockquote.java
@@ -0,0 +1,24 @@
+/*
+ * Blockquote.java
+ *
+ * Created on 28 August 2006, 23:25
+ *
+ * To change this template, choose Tools | Template Manager
+ * and open the template in the editor.
+ */
+
+package org.bloggers4labour.tag;
+
+/**
+ *
+ * @author andrewre
+ */
+public class Blockquote extends Tag
+{
+	/********************************************************************
+	********************************************************************/
+	public Blockquote( int inStartPos, int inEndPos, String inName)
+	{
+		super( inStartPos, inEndPos, inName);
+	}
+}
