7d7fc36dd87827e7d7a0cbaab8c68172fcb85eb1
Andrew Regan <aregan@gmail.com>
2006-12-29 22:38:00 +0000

    Nov 2006. Removed some stuff now in Score.java
    git-svn-id: file:///Users/andrewregan/Development/SVN/repos/trunk/Bloggers4Labour@105 aa2b285a-0eb0-49ca-9bdb-c93b1c50f45f

--- src/org/bloggers4labour/cricket/LogUpdatesHandler.java
+++ src/org/bloggers4labour/cricket/LogUpdatesHandler.java
@@ -10,7 +10,6 @@
 package org.bloggers4labour.cricket;
 
 import de.nava.informa.core.ItemIF;
-import java.util.regex.*;
 import org.apache.log4j.Logger;
 import org.bloggers4labour.*;
 import org.bloggers4labour.headlines.AddHandler;
@@ -23,20 +22,15 @@ import org.bloggers4labour.jsp.DisplayItem;
  */
 public class LogUpdatesHandler implements AddHandler
 {
-	private static Pattern	s_1Day1stInningsPattern = Pattern.compile("([A-Za-z]* *[A-Za-z]+) (.*) v ([A-Za-z]* *[A-Za-z]+)$");
-	private static Pattern	s_MatchPattern = Pattern.compile("([A-Za-z]* *[A-Za-z]+) (.*) v ([A-Za-z]* *[A-Za-z]+) ([0-9\\-]*)");
-
 	/*******************************************************************************
 	*******************************************************************************/
 	public void onAdd( final Installation inInstall, HeadlinesIF inHeads, final ItemIF inItem, final ItemContext inCtxt)
 	{
 		DisplayItem	d = new DisplayItem( inInstall, inItem, System.currentTimeMillis());
 		String		channelStr = FeedUtils.channelToString( inItem.getChannel() );
-	//	String		scoreStr = inItem.getTitle();
 		String		scoreStr = inItem.getDescription().trim();
-	//	Matcher		m = s_MatchPattern.matcher(scoreStr);
 		Score		theScore = Score.parse( inItem.getDescription().trim() );
 
-		Logger.getLogger("Main").info("... New post @ " + new java.util.Date() + " ... " + FeedUtils.getItemDate(inItem)  + " ... " + d.getDateString() + " ... " + theScore);
+		Logger.getLogger("B4L_NewPosts").info("... New post @ " + new java.util.Date() + " ... " + FeedUtils.getItemDate(inItem)  + " ... " + d.getDateString() + " ... " + theScore);
 	}
 }
